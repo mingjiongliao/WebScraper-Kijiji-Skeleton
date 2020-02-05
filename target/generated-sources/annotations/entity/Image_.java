8af7eaac6278d9bb0b32e85b12aeaf3c656af5df
Mingjiong liao <22457102+mingjiongliao@users.noreply.github.com>
2020-02-05 14:49:45 -0500

    Update Image_.java
    create a new test entry

--- target/generated-sources/annotations/entity/Image_.java
+++ target/generated-sources/annotations/entity/Image_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-28T16:12:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-05T13:47:37")
 @StaticMetamodel(Image.class)
 public class Image_ { 
 
