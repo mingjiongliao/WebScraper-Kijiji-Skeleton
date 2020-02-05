0dd5ed322a44b2acb9b7ff53e0eb143b98513d42
Mingjiong liao <22457102+mingjiongliao@users.noreply.github.com>
2020-02-05 14:47:36 -0500

    Create ImageTableView.java
    done

---
+++ src/main/java/view/ImageTableView.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import entity.Image;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import logic.ImageLogic;
+
+/**
+ *
+ * @author mingjiongliao
+ */
+@WebServlet(name = "ImageTable", urlPatterns = {"/ImageTable"})
+public class ImageTableView extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Image Table View</title>");            
+            out.println("</head>");
+            out.println("<body>");
+             out.println("<table style=\"margin-left: auto; margin-right: auto;\" border=\"1\">");
+            out.println("<caption>Image</caption>");
+            out.println("<tr>");
+            out.println("<th>ID</th>");
+            out.println("<th>URL</th>");
+            out.println("<th>Path</th>");
+            out.println("<th>Name</th>");
+            out.println("</tr>");
+
+            ImageLogic logic = new ImageLogic();
+            List<Image> entities = logic.getAll();
+            for (Image e : entities) {
+                //for other tables replace the code bellow with
+                //extractDataAsList in a loop to fill the data.
+                out.printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
+                        logic.extractDataAsList(e).toArray());
+            }
+
+            out.println("<tr>");
+            //this is an example, for your other tables use getColumnNames from
+            //logic to create headers in a loop.
+            out.println("<th>ID</th>");
+            out.println("<th>URL</th>");
+            out.println("<th>Path</th>");
+            out.println("<th>Name</th>");
+            out.println("</tr>");
+            out.println("</table>");
+            out.printf("<div style=\"text-align: center;\"><pre>%s</pre></div>", toStringMap(request.getParameterMap()));
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    private String toStringMap(Map<String, String[]> m) {
+            StringBuilder builder = new StringBuilder();
+            for (String k : m.keySet()) {
+                builder.append("Key=").append(k)
+                        .append(", ")
+                        .append("Value/s=").append(Arrays.toString(m.get(k)))
+                        .append(System.lineSeparator());
+            }
+            return builder.toString();
+        }   
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
