0bc52064855d8106242ed259ccb678556f155803
Mingjiong liao <22457102+mingjiongliao@users.noreply.github.com>
2020-02-18 11:04:54 -0500

    Create KijijiItem.java
    create

---
+++ src/main/java/scraper/kijiji/KijijiItem.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scraper.kijiji;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author mingjiongliao
+ */
+public class KijijiItem {
+
+    @Override
+    public String toString() {
+        return "KijijiItem{" + "id=" + id + '}';
+    }
+
+    public KijijiItem() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final KijijiItem other = (KijijiItem) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    private String id;
+    private String url;
+    private String imageUrl;
+    private String imageName;
+    private String price;
+    private String title;
+    private String date;
+    private String location;
+    private String description;
+    
+    
+    
+}
