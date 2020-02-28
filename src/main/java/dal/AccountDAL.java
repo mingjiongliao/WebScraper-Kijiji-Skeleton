269303e85fb83a80e3c9fbc4820ae20b6c265903
Mingjiong liao <22457102+mingjiongliao@users.noreply.github.com>
2020-02-28 11:35:01 -0500

    Update AccountDAL.java
    formated

--- src/main/java/dal/AccountDAL.java
+++ src/main/java/dal/AccountDAL.java
@@ -9,50 +9,50 @@ import java.util.Map;
  *
  * @author Shariar (Shawn) Emami
  */
-public class AccountDAL extends GenericDAL<Account>{
+public class AccountDAL extends GenericDAL<Account> {
 
     public AccountDAL() {
-        super( Account.class);
+        super(Account.class);
     }
-    
-    public List<Account> findAll(){
-        return findResults( "Account.findAll", null);
+
+    public List<Account> findAll() {
+        return findResults("Account.findAll", null);
     }
-    
-    public Account findById( int id){
+
+    public Account findById(int id) {
         Map<String, Object> map = new HashMap<>();
         map.put("id", id);
-        return findResult( "Account.findById", map);
+        return findResult("Account.findById", map);
     }
-    
-    public Account findByDisplayName( String displayName){
+
+    public Account findByDisplayName(String displayName) {
         Map<String, Object> map = new HashMap<>();
         map.put("displayName", displayName);
-        return findResult( "Account.findByDisplayName", map);
+        return findResult("Account.findByDisplayName", map);
     }
-    
-    public Account findByUser( String user){
+
+    public Account findByUser(String user) {
         Map<String, Object> map = new HashMap<>();
         map.put("user", user);
-        return findResult( "Account.findByUser", map);
+        return findResult("Account.findByUser", map);
     }
-    
-    public List<Account> findByPassword( String password){
+
+    public List<Account> findByPassword(String password) {
         Map<String, Object> map = new HashMap<>();
         map.put("password", password);
-        return findResults( "Account.findByPassword", map);
+        return findResults("Account.findByPassword", map);
     }
-    
-    public List<Account> findContaining(String search){
+
+    public List<Account> findContaining(String search) {
         Map<String, Object> map = new HashMap<>();
         map.put("search", search);
-        return findResults( "Account.findContaining", map);
+        return findResults("Account.findContaining", map);
     }
-    
-    public Account validateUser(String username, String pass){
+
+    public Account validateUser(String username, String pass) {
         Map<String, Object> map = new HashMap<>();
         map.put("username", username);
         map.put("password", pass);
-        return findResult( "Account.validateUser", map);
+        return findResult("Account.validateUser", map);
     }
 }
