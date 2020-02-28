0827cdfdbc6fcbaf0bfd3ed624870d53c674f4ca
Mingjiong liao <22457102+mingjiongliao@users.noreply.github.com>
2020-02-28 11:28:44 -0500

    Update Kijiji.java
    formated

--- src/main/java/scraper/kijiji/Kijiji.java
+++ src/main/java/scraper/kijiji/Kijiji.java
@@ -26,8 +26,7 @@ public class Kijiji {
         doc = Jsoup.connect(kijijiUrl).get();
         return this;
     }
-    
-    
+
     @Deprecated
     public Kijiji downloadDefaultPage() throws IOException {
         return downloadPage(URL_NEWEST_COMPUTER_ACCESSORIES);
@@ -37,18 +36,18 @@ public class Kijiji {
         itemElements = doc.getElementsByAttribute(ATTRIBUTE_ID);
         return this;
     }
-    
-    public Kijiji proccessItems(Consumer<KijijiItem> callback){
-        itemElements.forEach((Element e)->
-                {
-                    callback.accept(new ItemBuilder().setElement(e).build());
-                }
+
+    public Kijiji proccessItems(Consumer<KijijiItem> callback) {
+        itemElements.forEach((Element e)
+                -> {
+            callback.accept(new ItemBuilder().setElement(e).build());
+        }
         );
         return this;
     }
-    
+
     /**
-     * 
+     *
      * @param callback
      * @return
      * @deprecated
@@ -56,22 +55,23 @@ public class Kijiji {
     @Deprecated
     public Kijiji proccessItemsNoneBuilder(Consumer<BadKijijiItem> callback) {
 
-  //      itemElements.forEach((Element element) -> {
-  //          callback.accept( new ItemBuilder().setElement(element).build());
-  //      });
-       for (Element element : itemElements) {
-           callback.accept( new BadKijijiItem(element));       }
+        //      itemElements.forEach((Element element) -> {
+        //          callback.accept( new ItemBuilder().setElement(element).build());
+        //      });
+        for (Element element : itemElements) {
+            callback.accept(new BadKijijiItem(element));
+        }
         return this;
     }
-    
+
     public static void main(String[] args) throws IOException {
-        
+
         Consumer<BadKijijiItem> saveItemsNoneBuilder = (BadKijijiItem item) -> {
             System.out.println(item);
         };
 
         Kijiji kijiji = new Kijiji();
-        
+
         kijiji.downloadDefaultPage();
         kijiji.findAllItems();
         kijiji.proccessItemsNoneBuilder(saveItemsNoneBuilder);
