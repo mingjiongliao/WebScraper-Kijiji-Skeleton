662edd3e2440ee06d6a908b06a3b877c8903a31d
Mingjiong liao <22457102+mingjiongliao@users.noreply.github.com>
2020-02-28 11:24:58 -0500

    Update ImageLogic.java
    formated

--- src/main/java/logic/ImageLogic.java
+++ src/main/java/logic/ImageLogic.java
@@ -17,51 +17,47 @@ import java.util.Map;
  * @author mingjiongliao
  */
 /**
- * 
-+PATH : String = "path"
-+NAME : String = "name"
-+URL : String = "url"
-+ID : String = "id"
-+ImageLogic()
-+getAll() : List<Image>
-+getWithId(id : int) : Image
-+getWithUrl(url : String) : List<Image>
-+getWithPath(path : String) : Image
-+getWithName(name : String) : List<Image>
-+search(search : String) : List<Image>
-+createEntity(parameterMap : Map<String, String[]>) : Image
-+getColumnNames() : List<String>
-+getColumnCodes() : List<String>
-+extractDataAsList(e : Image) : List<?>
+ *
+ * +PATH : String = "path" +NAME : String = "name" +URL : String = "url" +ID :
+ * String = "id" +ImageLogic() +getAll() : List<Image>
+ * +getWithId(id : int) : Image +getWithUrl(url : String) : List<Image>
+ * +getWithPath(path : String) : Image +getWithName(name : String) : List<Image>
+ * +search(search : String) : List<Image>
+ * +createEntity(parameterMap : Map<String, String[]>) : Image +getColumnNames()
+ * : List<String>
+ * +getColumnCodes() : List<String>
+ * +extractDataAsList(e : Image) : List<?>
  */
-public class ImageLogic extends GenericLogic<Image,ImageDAL>{
+public class ImageLogic extends GenericLogic<Image, ImageDAL> {
+
     public static final String NAME = "name";
     public static final String URL = "url";
     public static final String ID = "id";
     public static final String PATH = "path";
+
     public ImageLogic() {
         super(new ImageDAL());
     }
-    
+
     @Override
     public List<String> getColumnNames() {
-        return Arrays.asList("ID","URL","PATH","NAME");
+        return Arrays.asList("ID", "URL", "PATH", "NAME");
     }
 
     @Override
     public List<String> getColumnCodes() {
-      return Arrays.asList(ID,URL,PATH,NAME);
+        return Arrays.asList(ID, URL, PATH, NAME);
     }
 
     @Override
     public List<?> extractDataAsList(Image e) {
-        return Arrays.asList(e.getId(),e.getUrl(),e.getPath(),e.getName());
+        return Arrays.asList(e.getId(), e.getUrl(), e.getPath(), e.getName());
     }
 
     @Override
     public Image createEntity(Map<String, String[]> parameterMap) {
         Image image = new Image();
-        if(parameterMap.containsKey(ID)){
+        if (parameterMap.containsKey(ID)) {
             image.setId(Integer.parseInt(parameterMap.get(ID)[0]));
         }
         image.setUrl(parameterMap.get(URL)[0]);
@@ -72,28 +68,28 @@ public class ImageLogic extends GenericLogic<Image,ImageDAL>{
 
     @Override
     public List<Image> getAll() {
-        return get(()->dao().findAll());
+        return get(() -> dao().findAll());
     }
 
     @Override
     public Image getWithId(int id) {
-        return get(()->dao().findById(id));
+        return get(() -> dao().findById(id));
     }
-    
+
     public List<Image> getWithUrl(String url) {
-        return get(()->dao().findByUrl(url));
+        return get(() -> dao().findByUrl(url));
     }
-    
+
     public Image getWithPath(String path) {
-        return get(()->dao().findByPath(path));
+        return get(() -> dao().findByPath(path));
     }
-    
+
     public List<Image> getWithName(String name) {
-        return get(()->dao().findByName(name));
+        return get(() -> dao().findByName(name));
     }
-    
-     public List<Image> search(String search){
-        return get(()->dao().findContaining(search));
+
+    public List<Image> search(String search) {
+        return get(() -> dao().findContaining(search));
     }
-     
- }
+
+}
